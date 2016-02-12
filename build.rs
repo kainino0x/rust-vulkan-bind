5c3f19f02b6cec95d5a5d4b689b3ae35b5c20a1b
Kai Ninomiya <kainino1@gmail.com>
2016-02-12 17:56:55 -0500

    Update to vulkan 1.0.2.0

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use std::process::Command;
 fn main() {
     Command::new("./bindgen/target/release/bindgen").args(
         &[
-            "-lvulkan-1",
+            "-lvulkan",
             "-o", "src/vulkan.rs",
             "/usr/include/vulkan/vulkan.h",
         ]).status().unwrap();

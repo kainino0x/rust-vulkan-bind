da836f3f768be7c08259056e0551b8b30ae4fb7c
Kai Ninomiya <kainino1@gmail.com>
2016-02-14 14:24:56 -0500

    Enable XCB in bindings

--- build.rs
+++ build.rs
@@ -4,7 +4,9 @@ fn main() {
     Command::new("./bindgen/target/release/bindgen").args(
         &[
             "-lvulkan",
+            "-DVK_USE_PLATFORM_XCB_KHR",
             "-o", "src/vulkan.rs",
+            "-match", "vulkan.h",
             "/usr/include/vulkan/vulkan.h",
         ]).status().unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
 #![allow(dead_code, non_camel_case_types, non_snake_case)]
 
+extern crate libc;
+extern crate xcb;
+
+use libc::*;
+use xcb::ffi::*;
+
 include!("vulkan.rs");
